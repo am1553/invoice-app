9d8b9d133c94b0aab3d71e885e9a3487a5c4bbe8
Aryan Mehta <aryanmehta2016@gmail.com>
2022-11-19 18:30:32 +0000

    fixed status card bg color

--- src/components/ui/Status.js
+++ src/components/ui/Status.js
@@ -1,17 +1,58 @@
-import { ThemeContext } from '@emotion/react'
 import React, { useContext } from 'react'
+import { ThemeContext } from '../../context/ThemeContext'
 
 function Status({status}) {
     const theme = useContext(ThemeContext)[0]
-    console.log(status)
+
+    const Circle = ({background}) => {
+      console.log(background)
+      return(
+        <div 
+        className={
+          `w-2 h-2 rounded bg-[${background}]`
+        }>
+        </div>
+      )
+    }
+
+    const Text = ({text, color}) => {
+      return(
+        <h4 className={`
+        capitalize 
+        text-h4 
+        font-bold 
+        ${status === "paid" ? "text-[#33D69F]" : 
+          status === "pending" ? "text-[#FF8F00]" :
+          status === "draft" && theme === "light" ? "text-[#373B53]" :
+          status === "draft" && theme === "dark" ? "text-[#0000000]" : null}`}
+        >
+            {status}
+        </h4>
+      )
+    }
+
   return (
     <div 
-        className={`${status === "paid" ? "bg-[#33D69F10]" : status === "pending" ? "bg-[#FF8F0010]" : theme === "light" ? "bg-[#373B5310]" : "bg-[#DFE3FA10]"} rounded-md flex items-center justify-center gap-2 w-28 h-10`}
+        className={`${
+            status === "paid" ? "bg-[#33D69F20]" 
+            : 
+            status === "pending" ? "bg-[#FF8F0020]" 
+            :
+            status === "draft" && theme === "light" ? "bg-[#373B5320]" : "bg-[#DFE3FA20]"
+          } rounded-md flex items-center justify-center gap-2 w-28 h-10`}
     >
-        <div className={`w-2 h-2 rounded ${status === "paid" ? "bg-[#33D69F]" : status === "pending" ? "bg-[#FF8F00]" : theme === "light" ? "bg-[#373B53]" : "bg-[#DFE3FA]"}`}></div>
+      {
+        status === "paid" ? <Circle background={'#33D69F'}/> 
+        : 
+        status === "draft" ? <Circle background={theme === "light" ? "#373B53" : "#DFE3FA"}/>
+        : <Circle background={'#FF8F00'}/> 
+      }
 
-        <h4 className={`capitalize text-h4 font-bold ${status === "paid" ? "text-[#33D69F]" : status === "pending" ? "text-[#FF8F00]" : theme === "light" ? "text-[#373B53]" : "text-[#DFE3FA]"}`}>{status}</h4>
-
+      {
+        status === "paid" ? <Text text={"paid"} color={"#33D69F"}/> :
+        status === "draft" ? <Text text={"draft"} color={theme === "light" ? "#373B53" : "#DFE3FA"}/> :
+        <Text text={"pending"} color={"#FF8F00"}/>
+      }
     </div>
   )
 }

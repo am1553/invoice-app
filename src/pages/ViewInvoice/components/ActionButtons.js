85a425e1b1b5c5cc4b0b4871ad2b2dd69818ccaf
Aryan Mehta <aryanmehta2016@gmail.com>
2022-11-20 18:38:32 +0000

    completed view invoice

--- src/pages/ViewInvoice/components/ActionButtons.js
+++ src/pages/ViewInvoice/components/ActionButtons.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {PurpleButton, DestructiveButton, GreyButton} from '../../../components/ui/Buttons'
 function ActionButtons() {
   return (
-    <footer className='bg-dark-greyish-blue mt-14 p-8 flex justify-center gap-2'>
+    <footer className='bg-dark-greyish-blue max-md:p-8 flex justify-center gap-2 md:bg-opacity-0'>
         <GreyButton text={"Edit"}/>
         <DestructiveButton text={"Delete"}/>
         <PurpleButton text={"Mark as Paid"}/>
--- src/pages/ViewInvoice/components/StatusCard.js
+++ src/pages/ViewInvoice/components/StatusCard.js
@@ -1,16 +1,20 @@
 import React, { useContext } from 'react'
 import Status from '../../../components/ui/Status'
 import { ThemeContext } from '../../../context/ThemeContext'
+import ActionButtons from './ActionButtons'
 import InvoiceDetails from './InvoiceDetails'
 
 function StatusCard({invoice}) {
   const theme = useContext(ThemeContext)[0]
 
   return (
-    <div className={`mt-10 ${theme === "light" ? "bg-white" : "bg-dark-greyish-blue"} p-6 rounded-lg flex items-center justify-between`}>
-        <div className="flex items-center justify-between w-full">
+    <div className={`${theme === "light" ? "bg-white" : "bg-dark-greyish-blue"} p-6 rounded-lg flex items-center md:grid md:grid-cols-[40%_60%] `}>
+        <div className="flex items-center justify-between w-full md:justify-start md:items-center md:gap-4 ">
             <span className='text-body-l'>Status</span>
             <Status status={invoice.status}/>
+        </div>
+        <div className="max-md:hidden">
+          <ActionButtons />
         </div>
     </div>
   )

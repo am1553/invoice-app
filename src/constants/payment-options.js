55badb67e24056f0cb13170e8938741bff877efe
Aryan Mehta <aryanmehta2016@gmail.com>
2022-11-21 05:28:37 +0000

    Updates

--- src/constants/payment-options.js
+++
@@ -1,6 +0,0 @@
-export const paymentTermOptions = [
-    {label: 'Net 1 Day', value: 1},
-    {label: 'Net 7 Day', value: 7},
-    {label: 'Net 14 Day', value: 14},
-    {label: 'Net 30 Day', value: 30}
-]
